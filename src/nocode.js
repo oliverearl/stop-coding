339b6b5cfb1dfbc250b496630e6796456f785587
Oliver Earl <oliver@oliverearl.co.uk>
2019-06-20 19:52:14 +0100

    App functionality finished

--- src/nocode.js
+++ src/nocode.js
@@ -19,16 +19,16 @@
   * Horrendously.
   * @param {Node} textNode 
   */
- const handleText = function(textNode) {
-     let value = textNode.nodeValue;
+ const replace = function(textNode) {
+    let value = textNode.nodeValue;
 
-     // Eurgh
-     value = value.replace(/\bcoding\b/g, "programming");
-     value = value.replace(/\bCoding\b/g, "Programming");
-     value = value.replace(/\bCODING\b/g, "PROGRAMMING");
+    // Eurgh
+    value = value.replace(/\bcoding\b/g, "programming");
+    value = value.replace(/\bCoding\b/g, "Programming");
+    value = value.replace(/\bCODING\b/g, "PROGRAMMING");
 
-     textNode.nodeValue = value;
- }
+    textNode.nodeValue = value;
+}
 
  /**
   * Walk function to Regex text outside of HTML attributes
@@ -37,32 +37,37 @@
   * https://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945
   * @param {Node} node 
   */
- const walk = function(node) {
-     let child, next;
+const walk = function(node) 
+{	
+    let child, next;
+    
+     // We don't want to affect input nodes, or error on null/undefined
+    if (node === null ||
+        node === undefined ||
+        node.nodeName.toLowerCase() == 'input' ||
+        node.nodeName.toLowerCase() == 'textarea' ||
+        (node.classList && node.classList.contains('ace_editor'))) {
+            return;
+        }
 
-     // We don't want to affect input nodes.
-     if (node.tagName.toLowerCase() === 'input' 
-     || node.tagName.toLowerCase() === 'textarea' 
-     || node.classList.indexOf('ace_editor') > -1) {
-         return;
-     }
-
-     switch(node.NodeType) {
-         case 1: // Element
-         case 9: // Document
-         case 11: // Document fragment
-            child = node.firstChild;
-            while (child) {
-                // Oh you know what's coming next.
+	switch (node.nodeType) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child) {
                 next = child.nextSibling;
-                walk(child);
-                child = next;
-            }
+                // Recursion is fun
+				walk(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			replace(node);
             break;
-        case 3: // Text node
-            handleText(node);
+        default:
             break;
-     }
- }
+	}
+}
 
 walk(document.body);
